936b79d923a99e4aceb617bc27989166faf85c68
MohamedBassem <medoox240@gmail.com>
2015-12-06 04:05:09 +0200

    Adding the deployment Part

---
+++ seniors-session-3-deployments-and-administration/resources/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+var counter = 1;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log("Got a new Request " + req.url);
+  res.end("You are vistor number : " + counter);
+  counter++;
+}).listen(8080);
